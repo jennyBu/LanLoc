5e7a2e9b5bf4d8614cb65aae302fb117a55ecafd
jennyBu <j.busta@online.de>
2017-12-03 20:39:45 +0100

    Database

---
+++ app/src/main/java/com/example/usi/lanloc/DatabaseActivity.java
@@ -0,0 +1,225 @@
+package com.example.usi.lanloc;
+
+import android.os.AsyncTask;
+import org.json.JSONArray;
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.net.URL;
+import java.net.URLConnection;
+import java.net.URLEncoder;
+import java.util.List;
+
+/**
+ * Created by Jennifer Busta on 01.12.17.
+ */
+public class DatabaseActivity extends AsyncTask {
+
+    @Override
+    protected Object doInBackground(Object[] objects) {
+        try {
+            String link = "http://uc-edu.mobile.usilu.net/lanloc.php";
+            String data = (String)objects[0];
+
+            URL url = new URL(link);
+            URLConnection conn = url.openConnection();
+
+            conn.setDoOutput(true);
+            OutputStreamWriter wr = new OutputStreamWriter(conn.getOutputStream());
+
+            wr.write( data );
+            wr.flush();
+
+            BufferedReader reader = new BufferedReader(new InputStreamReader(conn.getInputStream()));
+
+            StringBuilder sb = new StringBuilder();
+            String line = null;
+            while((line = reader.readLine()) != null) {
+                sb.append(line);
+                break;
+            }
+
+            return new JSONArray(line);
+            /*JSONArray jsonarray = new JSONArray(line);
+            for (int i = 0; i < jsonarray.length(); i++) {
+                JSONObject jsonobject = jsonarray.getJSONObject(i);
+                String android_id = jsonobject.getString("android_id");
+            }
+
+            return sb.toString();
+
+            */
+        } catch (Exception e) {
+            return new String("Exception: " + e.getMessage());
+        }
+    }
+
+    /**
+     *
+     * @param androidId the device id of the phone
+     * @return the user id. If not already in database it is added and then returned.
+     */
+    public Object getUserByAndroidId(String androidId) {
+        try {
+            String data = URLEncoder.encode("method", "UTF-8") + "=" + URLEncoder.encode("getUserByAndroidId", "UTF-8");
+            data += "&" + URLEncoder.encode("android_id", "UTF-8") + "=" + URLEncoder.encode(androidId, "UTF-8");
+
+            return execute(data);
+        } catch (Exception e) {
+            return new String("Exception: " + e.getMessage());
+        }
+    }
+
+    /**
+     *
+     * @param latitude of the position to get
+     * @param longitude of the position to get
+     * @return the id of the position. If not already in database it is added and then returned.
+     */
+    public Object getPositionId(Double latitude, Double longitude) {
+        String lat = Double.toString(latitude);
+        String lon = Double.toString(longitude);
+
+        try {
+            String data = URLEncoder.encode("method", "UTF-8") + "=" + URLEncoder.encode("getPositionId", "UTF-8");
+            data += "&" + URLEncoder.encode("latitude", "UTF-8") + "=" + URLEncoder.encode(lat, "UTF-8");
+            data += "&" + URLEncoder.encode("longitude", "UTF-8") + "=" + URLEncoder.encode(lon, "UTF-8");
+
+            return execute(data);
+        } catch (Exception e) {
+            return new String("Exception: " + e.getMessage());
+        }
+    }
+
+    /**
+     *
+     * @param latitude of the current position
+     * @param longitude of the current position
+     * @param distance to the current position in meters
+     * @return all positions in the range of the distance around the current position
+     */
+    public Object getAllPositionsInRange(Double latitude, Double longitude, Integer distance) {
+        String lat = Double.toString(latitude);
+        String lon = Double.toString(longitude);
+        String dist = distance.toString();
+
+        try {
+            String data = URLEncoder.encode("method", "UTF-8") + "=" + URLEncoder.encode("getAllPositionsInRange", "UTF-8");
+            data += "&" + URLEncoder.encode("latitude", "UTF-8") + "=" + URLEncoder.encode(lat, "UTF-8");
+            data += "&" + URLEncoder.encode("longitude", "UTF-8") + "=" + URLEncoder.encode(lon, "UTF-8");
+            data += "&" + URLEncoder.encode("distance", "UTF-8") + "=" + URLEncoder.encode(dist, "UTF-8");
+
+            return execute(data);
+        } catch (Exception e) {
+            return new String("Exception: " + e.getMessage());
+        }
+    }
+
+    /**
+     *
+     * @param positions that are searched for
+     * @return records for all mentioned positions
+     */
+    public Object getRecordsForPositions(List<Integer> positions) {
+        String formattedPositions = formatPositions(positions);
+
+        try {
+            String data = URLEncoder.encode("method", "UTF-8") + "=" + URLEncoder.encode("getRecordsForPositions", "UTF-8");
+            data += "&" + URLEncoder.encode("positions", "UTF-8") + "=" + URLEncoder.encode(formattedPositions, "UTF-8");
+
+            return execute(data);
+        } catch (Exception e) {
+            return new String("Exception: " + e.getMessage());
+        }
+    }
+
+    private String formatPositions(List<Integer> positions) {
+        String result = positions.toString();
+        result = result.replace("[", "(");
+        result = result.replace("]", ")");
+
+        return result;
+    }
+
+    // TODO for the following function no return (should doInBackground be adapted?)
+
+    /**
+     *
+     * @param userId foreign key for user (id column in user table)
+     * @param positionId foreign key for position (id column in positions table)
+     * @param path path to the audio file on file server
+     */
+    public void addRecord(Integer userId, Integer positionId, String path) {
+        String user = userId.toString();
+        String position = positionId.toString();
+
+        try {
+            String data = URLEncoder.encode("method", "UTF-8") + "=" + URLEncoder.encode("addRecord", "UTF-8");
+            data += "&" + URLEncoder.encode("user", "UTF-8") + "=" + URLEncoder.encode(user, "UTF-8");
+            data += "&" + URLEncoder.encode("position", "UTF-8") + "=" + URLEncoder.encode(position, "UTF-8");
+            data += "&" + URLEncoder.encode("audio", "UTF-8") + "=" + URLEncoder.encode(path, "UTF-8");
+
+            execute(data);
+        } catch (Exception e) {
+            //return new String("Exception: " + e.getMessage());
+        }
+    }
+
+    /**
+     *
+     * @param recordId id of the record that should get an upvote
+     */
+    public void voteRecordUp(Integer recordId) {
+        String record = recordId.toString();
+
+        try {
+            String data = URLEncoder.encode("method", "UTF-8") + "=" + URLEncoder.encode("voteRecordUp", "UTF-8");
+            data += "&" + URLEncoder.encode("record", "UTF-8") + "=" + URLEncoder.encode(record, "UTF-8");
+
+            execute(data);
+        } catch (Exception e) {
+            //return new String("Exception: " + e.getMessage());
+        }
+    }
+
+    /**
+     * @param recordId id of the record that should get a downvote
+     */
+    public void voteRecordDown(Integer recordId) {
+        String record = recordId.toString();
+
+        try {
+            String data = URLEncoder.encode("method", "UTF-8") + "=" + URLEncoder.encode("voteRecordDown", "UTF-8");
+            data += "&" + URLEncoder.encode("record", "UTF-8") + "=" + URLEncoder.encode(record, "UTF-8");
+
+            execute(data);
+        } catch (Exception e) {
+            //return new String("Exception: " + e.getMessage());
+        }
+    }
+
+    /*public Object getUserById(String id) {
+        try {
+            String data = URLEncoder.encode("method", "UTF-8") + "=" + URLEncoder.encode("getUserByAndroidId", "UTF-8");
+            data += "&" + URLEncoder.encode("id", "UTF-8") + "=" + URLEncoder.encode(id, "UTF-8");
+
+            return execute(data);
+        } catch (Exception e) {
+            return new String("Exception: " + e.getMessage());
+        }
+    }*/
+
+    /*
+
+    User posts something:
+    - check if android id is already in database and get user id for it         DONE
+    - check if position is already in database and get position id for it       DONE
+    - create new record using user id and position id                           DONE
+    - increase upvotes and downvotes                                            DONE
+
+    User wants to see and play records around him:
+    - get all position ids from db in a specific range                          DONE
+    - get all records for position ids                                          DONE
+
+     */
+}
--- app/src/main/java/com/example/usi/lanloc/MainActivity.java
+++ app/src/main/java/com/example/usi/lanloc/MainActivity.java
@@ -1,10 +1,11 @@
 package com.example.usi.lanloc;
 
-import android.app.ListActivity;
 import android.os.Bundle;
 import android.support.design.widget.TabLayout;
 import android.support.v4.view.ViewPager;
 import android.support.v7.app.AppCompatActivity;
+import java.util.ArrayList;
+import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -12,6 +13,20 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        //new DatabaseActivity().execute("getUserByAndroidId", "222333444");
+        //new DatabaseActivity().getUserByAndroidId("222333444");
+        //new DatabaseActivity().getUserById("1");
+        //new DatabaseActivity().getPositionId(123.222, 345.222);
+        //new DatabaseActivity().addRecord(1, 1, "apath/to/somewhere/else");
+        //new DatabaseActivity().voteRecordDown(4);
+        //new DatabaseActivity().voteRecordUp(2);
+        //new DatabaseActivity().getAllPositionsInRange(46.010475, 8.957066, 1000);
+        //List<Integer> positions = new ArrayList<>();
+        //positions.add(1);
+        //positions.add(2);
+        //positions.add(5);
+        //new DatabaseActivity().getRecordsForPositions(positions);
 
         ViewPager viewPager = (ViewPager) findViewById(R.id.viewpager);
         viewPager.setAdapter(new CustomPagerAdapter(this));
