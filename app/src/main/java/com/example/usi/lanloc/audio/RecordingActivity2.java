940096a2e41c8ee4c9a85a0ee97d83ac0d8f35a1
unknown <milo.salaveni@gemarship.ch>
2017-12-10 18:46:15 +0100

    uploads trouble

--- app/src/main/java/com/example/usi/lanloc/audio/RecordingActivity2.java
+++ app/src/main/java/com/example/usi/lanloc/audio/RecordingActivity2.java
@@ -75,16 +75,6 @@ public class RecordingActivity2 extends AppCompatActivity {
         backview = (ImageView) findViewById(R.id.back_icon);
         uploadview = (ImageView) findViewById(R.id.upload_icon);
 
-
-        uploadview.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-             //   doFileUpload();
-
-            }
-        });
-
-
         backview.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
@@ -95,6 +85,20 @@ public class RecordingActivity2 extends AppCompatActivity {
 
             }
         });
+
+        uploadview.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                //        Toast.makeText(MainActivity.this, "Recording",
+                //              Toast.LENGTH_LONG).show();
+//                startActivity(new Intent(MainActivity.this, RecordingActivity.class));
+                doFileUpload();
+
+            }
+        });
+
+
+
 
         ToggleButton toggle = (ToggleButton) findViewById(R.id.toggle);
         toggle.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
@@ -214,10 +218,6 @@ public class RecordingActivity2 extends AppCompatActivity {
                 }
             }
         });
-        mediaPlayer.release();
-        MediaRecorderReady();
-        toggle2.toggle();
-
 
 
 
@@ -263,91 +263,6 @@ public class RecordingActivity2 extends AppCompatActivity {
         }); W*/
 
 
-    }
-
-
-
-    private void doFileUpload(){
-        HttpURLConnection conn = null;
-        DataOutputStream dos = null;
-        DataInputStream inStream = null;
-        String lineEnd = "rn";
-        String twoHyphens = "--";
-        String boundary =  "*****";
-        int bytesRead, bytesAvailable, bufferSize;
-        byte[] buffer;
-        int maxBufferSize = 1*1024*1024;
-        String responseFromServer = "";
-        String urlString = "http://uc-edu.mobile.usilu.net/audio2.php";
-        try
-        {
-            //------------------ CLIENT REQUEST
-            FileInputStream fileInputStream = new FileInputStream(new File(AudioSavePathInDevice1) );
-            // open a URL connection to the Servlet
-            URL url = new URL(urlString);
-            // Open a HTTP connection to the URL
-            conn = (HttpURLConnection) url.openConnection();
-            // Allow Inputs
-            conn.setDoInput(true);
-            // Allow Outputs
-            conn.setDoOutput(true);
-            // Don't use a cached copy.
-            conn.setUseCaches(false);
-            // Use a post method.
-            conn.setRequestMethod("POST");
-            conn.setRequestProperty("Connection", "Keep-Alive");
-            conn.setRequestProperty("Content-Type", "multipart/form-data;boundary="+boundary);
-            dos = new DataOutputStream( conn.getOutputStream() );
-            dos.writeBytes(twoHyphens + boundary + lineEnd);
-       //     dos.writeBytes("Content-Disposition: form-data; name="AudioSavePathInDevice";filename="" + selectedPath + """ + lineEnd)
-            dos.writeBytes("Content-Disposition: form-data; name=\"uploadedfile\";filename=\"" + AudioSavePathInDevice1 + "" + lineEnd);
-
-            dos.writeBytes(lineEnd);
-            // create a buffer of maximum size
-            bytesAvailable = fileInputStream.available();
-            bufferSize = Math.min(bytesAvailable, maxBufferSize);
-            buffer = new byte[bufferSize];
-            // read file and write it into form...
-            bytesRead = fileInputStream.read(buffer, 0, bufferSize);
-            while (bytesRead > 0)
-            {
-                dos.write(buffer, 0, bufferSize);
-                bytesAvailable = fileInputStream.available();
-                bufferSize = Math.min(bytesAvailable, maxBufferSize);
-                bytesRead = fileInputStream.read(buffer, 0, bufferSize);
-            }
-            // send multipart form data necesssary after file data...
-            dos.writeBytes(lineEnd);
-            dos.writeBytes(twoHyphens + boundary + twoHyphens + lineEnd);
-            // close streams
-            Log.e("Debug","File is written");
-            fileInputStream.close();
-            dos.flush();
-            dos.close();
-        }
-        catch (MalformedURLException ex)
-        {
-            Log.e("Debug", "error: " + ex.getMessage(), ex);
-        }
-        catch (IOException ioe)
-        {
-            Log.e("Debug", "error: " + ioe.getMessage(), ioe);
-        }
-        //------------------ read the SERVER RESPONSE
-        try {
-            inStream = new DataInputStream ( conn.getInputStream() );
-            String str;
-
-            while (( str = inStream.readLine()) != null)
-            {
-                Log.e("Debug","Server Response "+str);
-            }
-            inStream.close();
-
-        }
-        catch (IOException ioex){
-            Log.e("Debug", "error: " + ioex.getMessage(), ioex);
-        }
     }
 
 
@@ -470,7 +385,96 @@ public class RecordingActivity2 extends AppCompatActivity {
         }
     }  */
 
+    private void doFileUpload() {
 
+        new AsyncTask<Void, Void, Void>() {
+
+            @Override
+            protected Void doInBackground(Void... params) {
+                //private void doFileUpload(){
+                HttpURLConnection conn = null;
+                DataOutputStream dos = null;
+                DataInputStream inStream = null;
+                String lineEnd = "rn";
+                String twoHyphens = "--";
+                String boundary = "*****";
+                int bytesRead, bytesAvailable, bufferSize;
+                byte[] buffer;
+                int maxBufferSize = 1 * 1024 * 1024;
+                String responseFromServer = "";
+                Log.d("function", "this is the value of selected path" + AudioSavePathInDevice);
+                String urlString = "http://uc-edu.mobile.usilu.net/audio2.php";
+                try {
+                    //------------------ CLIENT REQUEST
+                    FileInputStream fileInputStream = new FileInputStream(new File(AudioSavePathInDevice));
+                    // open a URL connection to the Servlet
+                    URL url = new URL(urlString);
+                    // Open a HTTP connection to the URL
+                    conn = (HttpURLConnection) url.openConnection();
+                    // Allow Inputs
+                    conn.setDoInput(true);
+                    // Allow Outputs
+                    conn.setDoOutput(true);
+                    // Don't use a cached copy.
+                    conn.setUseCaches(false);
+                    // Use a post method.
+                    conn.setRequestMethod("POST");
+                    conn.setRequestProperty("Connection", "Keep-Alive");
+                    conn.setRequestProperty("Content-Type", "multipart/form-data;boundary=" + boundary);
+                    dos = new DataOutputStream(conn.getOutputStream());
+                    dos.writeBytes(twoHyphens + boundary + lineEnd);
+                    dos.writeBytes("Content-Disposition: form-data; name=\"uploadedfile\";filename=\"" + AudioSavePathInDevice + "" + lineEnd);
+
+                    //dos.writeBytes("Content-Disposition: form-data; name="uploadedfile";filename="" + selectedPath + """ + lineEnd);
+                    dos.writeBytes(lineEnd);
+                    // create a buffer of maximum size
+                    bytesAvailable = fileInputStream.available();
+                    bufferSize = Math.min(bytesAvailable, maxBufferSize);
+                    buffer = new byte[bufferSize];
+                    // read file and write it into form...
+                    bytesRead = fileInputStream.read(buffer, 0, bufferSize);
+                    while (bytesRead > 0) {
+                        dos.write(buffer, 0, bufferSize);
+                        bytesAvailable = fileInputStream.available();
+                        bufferSize = Math.min(bytesAvailable, maxBufferSize);
+                        bytesRead = fileInputStream.read(buffer, 0, bufferSize);
+                    }
+                    // send multipart form data necesssary after file data...
+                    dos.writeBytes(lineEnd);
+                    dos.writeBytes(twoHyphens + boundary + twoHyphens + lineEnd);
+                    // close streams
+                    Log.e("Debug", "File is written");
+                    fileInputStream.close();
+                    dos.flush();
+                    dos.close();
+                } catch (MalformedURLException ex) {
+                    Log.e("Debug", "error: " + ex.getMessage(), ex);
+                } catch (IOException ioe) {
+                    Log.e("Debug", "error: " + ioe.getMessage(), ioe);
+                }
+                //------------------ read the SERVER RESPONSE
+                try {
+                    inStream = new DataInputStream(conn.getInputStream());
+                    String str;
+
+                    while ((str = inStream.readLine()) != null) {
+                        Log.e("Debug", "Server Response " + str);
+                    }
+                    inStream.close();
+
+                } catch (IOException ioex) {
+                    Log.e("Debug", "error: " + ioex.getMessage(), ioex);
+                }
+                return null;
+            }
+
+            protected void onPostExecute(Void feed) {
+
+            }
+
+        }.execute();
+
+    }
 
     public void MediaRecorderReady(){
         mediaRecorder=new MediaRecorder();
